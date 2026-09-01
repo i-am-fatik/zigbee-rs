32aabaa4fe100e8283b1185cb40d159efdf93dfd
i-am-fatik <223427743+i-am-fatik@users.noreply.github.com>
2026-09-02 01:27:01 +0200

    chore(bdb): rustfmt

--- zigbee-base-device-behavior/src/lib.rs
+++ zigbee-base-device-behavior/src/lib.rs
@@ -182,9 +182,7 @@ impl BaseDeviceBehavior {
         // BDB 7.1 step 4: rejoin on the current channel, so no scan channels
         log::debug!("[BDB] step 4: attempt NWK rejoin");
         let remembered_extended_pan_id = IeeeAddress(*nib.extended_panid());
-        let confirm = device
-            .rejoin(remembered_extended_pan_id, 0..0, 0)
-            .await?;
+        let confirm = device.rejoin(remembered_extended_pan_id, 0..0, 0).await?;
 
         // BDB 7.1 step 5
         if confirm.status == NlmeJoinStatus::Success {
